ef4ad03b942b6f6dbef299ca9f33d50d00edd0f8
MartinaTrajkovic <44097334+MartinaTrajkovic@users.noreply.github.com>
2019-05-02 21:14:14 +0200

    Add files via upload

---
+++ scripts/themes.js
@@ -0,0 +1,31 @@
+const chooseThemeButton = document.querySelector("#chooseTheme");
+const themeBoxes = document.querySelectorAll(".themeBox");
+
+
+setBg("pink","bgPink");
+setBg("blue","bgBlue");
+setBg("orange","bgOrange");
+setBg("rose","bgRose");
+
+themeBoxes.forEach(box => box.addEventListener("click", chooseTheme));
+//this function changes CSS based on choosen theme
+function chooseTheme(){
+    let themeId = this.id;
+    document.querySelector("#themeLink").href = `css/${themeId}.css`;
+    localStorage.setItem("theme", `css/${themeId}.css`);
+}
+
+chooseThemeButton.addEventListener("click", function(){
+    changeDisplay("themes","none");
+    changeDisplay("categories", "grid");
+});
+
+if(localStorage.getItem("theme")){
+    document.querySelector("#themeLink").href = localStorage.getItem("theme");
+}
+
+
+
+function setBg(id,bg){
+    document.querySelector(`#${id}`).style.backgroundImage = `url(media/img/${bg}.svg)`;
+}
