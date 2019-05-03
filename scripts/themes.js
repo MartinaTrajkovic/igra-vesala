1daeb30496c07531aa4fa5d6bed0b7e7f4ec0c8d
MartinaTrajkovic <44097334+MartinaTrajkovic@users.noreply.github.com>
2019-05-04 00:51:24 +0200

    Update themes.js

--- scripts/themes.js
+++ scripts/themes.js
@@ -1,11 +1,9 @@
 const chooseThemeButton = document.querySelector("#chooseTheme");
 const themeBoxes = document.querySelectorAll(".themeBox");
-
 setBg("pink","bgPink");
 setBg("blue","bgBlue");
 setBg("orange","bgOrange");
 setBg("rose","bgRose");
-
 themeBoxes.forEach(box => box.addEventListener("click", chooseTheme));
 //this function changes CSS based on choosen theme
 function chooseTheme(){
@@ -13,20 +11,17 @@ function chooseTheme(){
     document.querySelector("#themeLink").href = `css/${themeId}.css`;
     localStorage.setItem("theme", `css/${themeId}.css`);
 }
-
 chooseThemeButton.addEventListener("click", function(){
     changeDisplay("themes","none");
     changeDisplay("categories", "grid");
     changeDisplay("chooseThemeAgain","flex")
 });
-
 if(localStorage.getItem("theme")){
     document.querySelector("#themeLink").href = localStorage.getItem("theme");
 }
 else{
   document.querySelector("#themeLink").href = `css/blue.css`;
 }
-
 function setBg(id,bg){
     document.querySelector(`#${id}`).style.backgroundImage = `url(media/img/${bg}.svg)`;
 }
